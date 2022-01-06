1e91adb63329f0f85c176a05d7a676c8e872c869
Francesco <francescocollotto@gmail.com>
2022-01-06 11:04:00 +0000

    complete exercise 1

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+let message = '';
+
+app.use(express.json());//middleware to parse json data to req.body
+
+
+app.get('/message', (req, res) => {
+  req.body.message = message;
+  res.send(req.body)
+})
+
+// use curl in terminal to post some data:
+// curl -XPOST http://localhost:3000/message -H 'content-type: application/json' -d '{ "message": "Hello, Makers" }'
+app.post('/message', (req, res) => {
+  message = req.body.message
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
